4bf1a7ccb3952f7025ae9f3ca8d98119c7b46729
ferhat <dmrfrht1@gmail.com>
2019-07-08 19:57:59 +0300

    added-contact-and-google-maps

---
+++ 2.Flexbox/2.Project-2/js/main.js
@@ -0,0 +1,9 @@
+// function initMap() {
+//   const loc = { lat: 41.008240, lng: 28.978359 }; // Istanbul
+//   const map = new google.maps.Map(document.querySelector('.map'), {
+//     zoom: 14,
+//     center: loc
+//   });
+//   const marker = new google.maps.Marker({ position: loc, map: map });
+// }
+
