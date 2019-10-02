4ddf4272df4eddf449d18c413a3d37e87858abe6
dmrfrht <dmrfrht1@gmail.com>
2019-10-02 23:05:41 +0300

    sidebar-popularBlogPost-newsLetter-footer

---
+++ 26.Project-29.blogger-website/README.md
@@ -0,0 +1,5 @@
+# Project 26 - Blogger Website
+
+![1](https://user-images.githubusercontent.com/30186772/66077447-b060bc80-e568-11e9-987b-cc916515f7a5.PNG)
+![2](https://user-images.githubusercontent.com/30186772/66077448-b060bc80-e568-11e9-95cf-7e6009fcba03.PNG)
+![3](https://user-images.githubusercontent.com/30186772/66077449-b060bc80-e568-11e9-982e-5c7b5a405454.PNG)
--- 26.Project-29.blogger-website/js/script.js
+++ 26.Project-29.blogger-website/js/script.js
@@ -1,3 +1,18 @@
+const responsive = {
+  0: {
+    items: 1
+  },
+  320: {
+    items: 1
+  },
+  560: {
+    items: 2
+  },
+  960: {
+    items: 3
+  }
+};
+
 $(document).ready(function () {
   $nav = $('.nav');
   $toggleCollapse = $('.toggle-collapse');
@@ -13,7 +28,18 @@ $(document).ready(function () {
     autoplayTimeout: 3000,
     dots: false,
     nav: true,
-    navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+    responsive: responsive
   });
+
+  // move up top
+  $('.move-up span').click(function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 2000)
+  });
+
+  // AOS
+  AOS.init();
 });
 
